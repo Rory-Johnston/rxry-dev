0abc39673c8dacb4e2b4dbdbe24f4b8b4b8ddee4
Rory-Johnston <business.rjohno@gmail.com>
2024-09-22 19:08:32 +0100

    fix(title): improved the title of the page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 const Header = () => {
   return (
-    <div className="h-[600px] flex flex-col justify-center items-center text-gray-950 text-center">
+    <div className="h-[600px] flex flex-col justify-center items-center text-gray-950 text-center overflow-visible">
       <div className="text-4xl font-medium mb-4">Rory Johnston</div>
-      <div className="text-6xl font-semibold">Full-stack engineer.</div>
+      <div className="text-7xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 leading-relaxed px-2">
+        Full-stack engineer.
+      </div>
     </div>
   );
 };
