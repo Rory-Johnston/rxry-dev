05710e32bd40cb1164438721596c4012a767280c
Rory-Johnston <business.rjohno@gmail.com>
2024-09-22 19:12:52 +0100

    feat(header): added background beams

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
+import { BackgroundBeams } from "../Background/Background";
+
 const Header = () => {
   return (
-    <div className="h-[600px] flex flex-col justify-center items-center text-gray-950 text-center overflow-visible">
+    <div className="h-screen flex flex-col justify-center items-center text-gray-950 text-center overflow-visible">
       <div className="text-4xl font-medium mb-4">Rory Johnston</div>
       <div className="text-7xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 leading-relaxed px-2">
         Full-stack engineer.
       </div>
+      <BackgroundBeams />
     </div>
   );
 };
