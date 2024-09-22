aca3480873dbe16c06740061bb2c0154e6640250
Rory-Johnston <business.rjohno@gmail.com>
2024-09-22 19:30:20 +0100

    feat(header): improved header w/ gradients and buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,30 @@ import { BackgroundBeams } from "../Background/Background";
 
 const Header = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-gray-950 text-center overflow-visible">
-      <div className="text-4xl font-medium mb-4">Rory Johnston</div>
-      <div className="text-7xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 leading-relaxed px-2">
-        Full-stack engineer.
+    <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-4xl mx-auto p-4">
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-white text-4xl font-bold relative z-10 text-center">
+          Rory Johnston
+        </p>
+        <div className="text-7xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 leading-relaxed px-2 text-center">
+          Full-stack engineer
+        </div>
+        <p></p>
+        <div className="flex justify-center items-center relative z-10 gap-5">
+          <a
+            href="#projects"
+            className="px-6 py-2 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white font-semibold rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:shadow-xl"
+          >
+            See my projects
+          </a>
+
+          <a
+            href="#about"
+            className="px-6 py-2 bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 text-white font-semibold rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:shadow-xl"
+          >
+            Learn more
+          </a>
+        </div>
       </div>
       <BackgroundBeams />
     </div>
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,9 @@
-import About from "../../components/About/About";
 import Header from "../../components/Header/Header";
-import Navigation from "../../components/Navigation/Navigation";
 
 const Main = () => {
   return (
     <div>
-      <Navigation />
       <Header />
-      <About />
     </div>
   );
 };
