db789765a1092dd0c262f17ca480781dfc2df7d4
Rory-Johnston <business.rjohno@gmail.com>
2024-09-22 22:22:03 +0100

    feat(techs): added techs - icons map to utils

---
+++ src/utils/techs.ts
@@ -0,0 +1,45 @@
+import {
+  SiReact,
+  SiFirebase,
+  SiMongodb,
+  SiTailwindcss,
+  SiJavascript,
+  SiTypescript,
+  SiHtml5,
+  SiCss3,
+  SiDjango,
+  SiFlask,
+  SiExpress,
+  SiPostgresql,
+  SiGit,
+  SiGithub,
+  SiDocker,
+  SiAmazon,
+  SiVercel,
+} from "react-icons/si";
+
+import { FaNodeJs, FaPython } from "react-icons/fa";
+
+const techIcons = {
+  React: SiReact,
+  Firebase: SiFirebase,
+  MongoDB: SiMongodb,
+  "Node.js": FaNodeJs,
+  Python: FaPython,
+  "Tailwind CSS": SiTailwindcss,
+  JavaScript: SiJavascript,
+  TypeScript: SiTypescript,
+  HTML5: SiHtml5,
+  CSS3: SiCss3,
+  Django: SiDjango,
+  Flask: SiFlask,
+  Express: SiExpress,
+  PostgreSQL: SiPostgresql,
+  Git: SiGit,
+  GitHub: SiGithub,
+  Docker: SiDocker,
+  AWS: SiAmazon,
+  Vercel: SiVercel,
+};
+
+export default techIcons;
