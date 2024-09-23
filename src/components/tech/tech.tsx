4036a1fc36b42ce2a10526261f7432eb1473e1e7
Rory-Johnston <business.rjohno@gmail.com>
2024-09-23 14:50:33 +0100

    fix(tech): improved styles of tech icons

---
+++ src/components/tech/tech.tsx
@@ -0,0 +1,17 @@
+import techIcons from "../../utils/techs";
+
+const TechIcon = ({ tech }) => {
+  const IconComponent = techIcons[tech];
+  return (
+    <div className="flex flex-col items-center">
+      {IconComponent ? (
+        <IconComponent size={20} className="text-white" title={tech} />
+      ) : (
+        <div className="w-12 h-12 bg-gray-700 rounded-full"></div>
+      )}
+      <span className="mt-2 text-sm text-white">{tech}</span>
+    </div>
+  );
+};
+
+export default TechIcon;
