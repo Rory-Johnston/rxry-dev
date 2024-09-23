a1f6599611230bd4b44640df02c1d291326c42cf
Rory-Johnston <business.rjohno@gmail.com>
2024-09-23 10:33:05 +0100

    fix(header): changed background colour

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { BackgroundBeams } from "../background/background";
 
 const Header = () => {
   return (
-    <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="h-screen w-full bg-neutral-900 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-4xl mx-auto p-4">
         <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-white text-4xl font-bold relative z-10 text-center">
           Rory Johnston
@@ -13,7 +13,7 @@ const Header = () => {
         <p></p>
         <div className="flex justify-center items-center relative z-10 gap-5">
           <a
-            href="#projects"
+            href="projects"
             className="px-6 py-2 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white font-semibold rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:shadow-xl"
           >
             See my projects
