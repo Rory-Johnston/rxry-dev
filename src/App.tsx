d8f6655cb68fd7d3cf8c8589cfaad79eb8b74157
Rory-Johnston <business.rjohno@gmail.com>
2024-09-23 19:19:01 +0100

    chore: app routing and new packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/main/main";
-import Projects from "./pages/projects/projects";
+import ProjectsOverview from "./pages/overview/overview";
+import BrickDetective from "./pages/detail/brick_detective";
+import ForzeFootball from "./pages/detail/forze_football";
+import Smashblade from "./pages/detail/smashblade";
+import Boilerplate from "./pages/detail/boilerplate";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<ProjectsOverview />} />
+        <Route path="/projects/brick-detective" element={<BrickDetective />} />
+        <Route path="/projects/forze-football" element={<ForzeFootball />} />
+        <Route path="/projects/smashblade" element={<Smashblade />} />
+        <Route path="/projects/boilerplate" element={<Boilerplate />} />
       </Routes>
     </Router>
   );
